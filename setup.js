4225bfbff6ef49ffe4150a2e58c1ce38583f7a6c
Dan Wahlin <danwahlin@Dans-MacBook-Pro.local>
2017-10-13 02:13:40 -0400

    Fix path

--- setup.js
+++ setup.js
@@ -89,8 +89,8 @@ downloadAndExtractProjects(function(err) {
     overlayFiles('./Labs/Component Properties and Angular Directives');
 
     //Lab 7
-    copyFolder('./Angular-JumpStart-master', './Labs/Services Providers and Http/Begin');
-    overlayFiles('./Labs/Services Providers and Http');
+    copyFolder('./Angular-JumpStart-master', './Labs/Services Providers and HttpClient/Begin');
+    overlayFiles('./Labs/Services Providers and HttpClient');
 
     //Lab 8
     copyFolder('./Angular-JumpStart-master', './Labs/Working with Routing/Begin');
