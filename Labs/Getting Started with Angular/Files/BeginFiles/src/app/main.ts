c085de13049af6b68febf70f0c19ccce04f22633
Dan Wahlin <danwahlin@Dans-iMac.local>
2017-11-01 23:19:27 -0700

    Update labs to Angular 5.0

--- Labs/Getting Started with Angular/Files/BeginFiles/src/app/main.ts
+++ Labs/Getting Started with Angular/Files/BeginFiles/src/app/main.ts
@@ -1,13 +1,4 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
+import { AppModule } from './app.module';
 
 //Bootstrap Code Goes Here
-
-
